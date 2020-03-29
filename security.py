2feb55636d8d62fa70b4ce332290d558d2c07db0
propellasallad <propellasallad@users.noreply.github.com>
2020-03-30 00:35:21 +1300

    Main Program
    Main program with security module template.

---
+++ security.py
@@ -0,0 +1,23 @@
+class Security():
+
+    # Initialize
+    def __init__(self):
+        pass
+
+    # Caesar Cipher Encryption
+    def CaesarEncrypt(self, text):
+        return
+
+    # Caesar Cipher Decryption
+    def CaesarDecrypt(self, text):
+        return
+
+    # Polyalphabetic Encrypt
+    def PolyEncrypt(self, text):
+        return
+    
+    # Polyalphabetic Decrypt
+    def PolyDecrypt(self, text):
+        return
+
+        
